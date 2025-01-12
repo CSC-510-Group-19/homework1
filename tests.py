dd8934c133ad09bafea49c0bc5362a1c70830c48
Willow Sapphire <willowsapphire@protonmail.com>
2025-01-12 21:11:01 +0000

    added comments to tests

--- tests.py
+++ tests.py
@@ -1,7 +1,9 @@
 from myfile import non_recursive_fib
 
+# tests that the fifth fib number is 3
 def test_fifth_num():
     assert non_recursive_fib(5) == 3
 
+# tests that the tenth fib number is 55 (should be 34. designed to fail for assignment)
 def test_tenth_num():
     assert non_recursive_fib(10) == 55
