a826cc179cf1fcbbf9f6fa187275299c52d99ad9
Willow Sapphire <willowsapphire@protonmail.com>
2025-01-12 20:29:37 +0000

    fixed syntax error

--- myfile.py
+++ myfile.py
@@ -1,6 +1,6 @@
-def non_recursivefib(num):
+def non_recursive_fib(num):
     last, curr = 0, 1
-    for ( in range(2, num + 1)):
+    for i in range(2, num + 1):
         print(last, end = ' ')
         last, curr = curr, last + curr
     print(curr)
