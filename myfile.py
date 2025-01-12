70d0577163d773e431fc2bab9e98189186fe539b
Willow Sapphire <willowsapphire@protonmail.com>
2025-01-12 20:43:21 +0000

    fixed bug in last fib number and created tests

--- myfile.py
+++ myfile.py
@@ -4,7 +4,7 @@ def non_recursive_fib(num):
     for i in range(2, num + 1):
         print(last, end = ' ')
         last, curr = curr, last + curr
-    print(curr)
-    return curr
+    print(last)
+    return last
 
 non_recursive_fib(10)
